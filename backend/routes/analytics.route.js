86379199887124e132413b46206e8b103a2aaa1c
BriAyo <abiadebrightcodes@gmail.com>
2026-03-30 17:49:44 +0100

    Initial backend setup

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+import { getAnalyticsData, getDailySalesData } from "../controllers/analytics.controller.js"
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute);
+
+export default router;
